f0879b7f9ae6bfffa3f5d87e1e72bc9d86f2c055
cral <dongshuai@ect888.com>
2017-04-01 17:25:37 +0800

    忘记密码

---
+++ app/src/main/java/com/optimumnano/quickcharge/activity/login/ForgetPwdActivity.java
@@ -0,0 +1,15 @@
+package com.optimumnano.quickcharge.activity.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.optimumnano.quickcharge.R;
+
+public class ForgetPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pwd);
+    }
+}
